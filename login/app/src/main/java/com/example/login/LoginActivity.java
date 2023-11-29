22a505f1614516e9de35b373886b0e5bf674b588
MD. SAKIBUL ISLAM <mdsakibulislam736@gmail.com>
2023-11-29 06:05:17 +0600

    a

--- login/app/src/main/java/com/example/login/LoginActivity.java
+++ login/app/src/main/java/com/example/login/LoginActivity.java
@@ -94,7 +94,7 @@ public class LoginActivity extends AppCompatActivity {
 
                     if(!Objects.equals(passwordFromDB, userPassword)){
                         loginUsername.setError(null);
-                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        Intent intent = new Intent(LoginActivity.this, home_page.class);
                         startActivity(intent);
                     }else{
                         loginPassword.setError("Invalid Credentials!");
---
+++ login/app/src/main/java/com/example/login/home_page.java
@@ -0,0 +1,14 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class home_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
