b53e18e3ccc8baf43b5b79c3eb4725bc8c918a73
MD. SAKIBUL ISLAM <mdsakibulislam736@gmail.com>
2023-12-11 05:17:00 +0600

    1

--- login/login/app/src/main/java/com/example/login/home_page.java
+++ login/login/app/src/main/java/com/example/login/home_page.java
@@ -10,7 +10,7 @@ import android.view.View;
 
 public class home_page extends AppCompatActivity {
 
-    CardView uploadNotice;
+    CardView uploadNotice,addGalleryImage;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +18,20 @@ public class home_page extends AppCompatActivity {
         setContentView(R.layout.activity_home_page);
 
         uploadNotice = findViewById(R.id.addNotice);
+        addGalleryImage = findViewById(R.id.addGalleryImage);
 
         uploadNotice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(home_page.this,UploadNotice.class);
+                startActivity(intent);
+            }
+        });
+
+        addGalleryImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(home_page.this,UploadImage.class);
                 startActivity(intent);
             }
         });
