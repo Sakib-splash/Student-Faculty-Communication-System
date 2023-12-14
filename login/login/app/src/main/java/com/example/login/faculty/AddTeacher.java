b472bf494368aafe84cfb5a36b177b3fc8de6145
MD. SAKIBUL ISLAM <mdsakibulislam736@gmail.com>
2023-12-14 22:49:34 +0600

    1

---
+++ login/login/app/src/main/java/com/example/login/faculty/AddTeacher.java
@@ -0,0 +1,16 @@
+package com.example.login.faculty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.login.R;
+
+public class AddTeacher extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_teacher);
+    }
+}
