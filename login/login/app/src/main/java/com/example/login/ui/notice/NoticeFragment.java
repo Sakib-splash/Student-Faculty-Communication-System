8c90194d78d6e80562a9e8df11b57835badd9d5d
MD. SAKIBUL ISLAM <mdsakibulislam736@gmail.com>
2024-01-09 21:59:45 +0600

    Notice Fragmentation and notice view to  user

--- login/login/app/src/main/java/com/example/login/ui/notice/NoticeFragment.java
+++ login/login/app/src/main/java/com/example/login/ui/notice/NoticeFragment.java
@@ -2,21 +2,85 @@ package com.example.login.ui.notice;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.login.R;
+import com.example.login.notice.DeleteNoticeActivity;
+import com.example.login.notice.NoticeAdapter;
+import com.example.login.notice.NoticeData;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 
 public class NoticeFragment extends Fragment {
 
+
+   private RecyclerView deleteNoticeRecycler;
+    private ProgressBar progressBar;
+    private ArrayList<NoticeData> list;
+    private  notice_Adapter adapter;
+    private  DatabaseReference reference;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notice, container, false);
+        View view = inflater.inflate(R.layout.fragment_notice, container, false);
+
+        deleteNoticeRecycler = view.findViewById(R.id.deleteNoticeRecycler);
+        progressBar = view.findViewById(R.id.progressBar);
+
+        reference = FirebaseDatabase.getInstance().getReference().child("Notice");
+
+        deleteNoticeRecycler.setLayoutManager(new LinearLayoutManager(getContext()));
+        deleteNoticeRecycler.setHasFixedSize(true);
+
+        getNotice();
+
+        return view;
+
+    }
+
+    private void getNotice() {
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                list = new ArrayList<>();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    NoticeData data = snapshot.getValue(NoticeData.class);
+                    list.add(data);
+                }
+                adapter = new notice_Adapter(getContext(), list);
+                adapter.notifyDataSetChanged();
+                progressBar.setVisibility(View.GONE);
+                deleteNoticeRecycler.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                progressBar.setVisibility(View.GONE);
+
+                Toast.makeText(getContext(), databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
