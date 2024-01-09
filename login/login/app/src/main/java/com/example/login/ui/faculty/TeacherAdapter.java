130b010bee14d3e365ecc343e9e6c2c1ff258ce6
MD. SAKIBUL ISLAM <mdsakibulislam736@gmail.com>
2024-01-10 02:41:26 +0600

    Faculty Fragmentation complete

---
+++ login/login/app/src/main/java/com/example/login/ui/faculty/TeacherAdapter.java
@@ -0,0 +1,81 @@
+package com.example.login.ui.faculty;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.login.R;
+import com.example.login.faculty.UpdateTeacherActivity;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class TeacherAdapter extends RecyclerView.Adapter<TeacherAdapter.TeacherViewAdapter>{
+
+    private List<TeacherData> list;
+    private Context context;
+    private String category;
+
+    public TeacherAdapter(List<TeacherData> list, Context context, String category) {
+        this.list = list;
+        this.context = context;
+        this.category = category;
+    }
+
+    @NonNull
+    @Override
+    public TeacherViewAdapter onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        boolean attachToRoot;
+        View view = LayoutInflater.from(context).inflate(R.layout.faculty_item_layout_user, parent,false);
+
+        return new TeacherViewAdapter(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TeacherViewAdapter holder, int position) {
+
+        TeacherData item = list.get(position);
+        holder.name.setText(item.getName());
+        holder.email.setText(item.getEmail());
+        holder.post.setText(item.getPost());
+
+        try {
+            Picasso.get().load(item.getImage()).into(holder.imageView);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class TeacherViewAdapter extends RecyclerView.ViewHolder {
+
+        private TextView name, email, post;
+
+        private ImageView imageView;
+
+        public TeacherViewAdapter(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.teacherName);
+            email = itemView.findViewById(R.id.teacherEmail);
+            post = itemView.findViewById(R.id.teacherPost);
+            imageView = itemView.findViewById(R.id.teacherImage);
+
+        }
+    }
+}
